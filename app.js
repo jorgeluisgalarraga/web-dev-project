bca036520ddc702339c8057feb709d9a4a1b93bb
Roman Devda <romandevda@Romans-MacBook-Pro.local>
2024-01-21 20:42:55 -0500

    Artists page render(error)

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 const artists = [
     {
-        photo: "",
-        name: "",
-        time:"",
-        genre: "",
-        popularTune: ""
+        photo: "http://harmandease.com/wp-content/uploads/2022/09/DSC9247-1-1-scaled.jpg",
+        name: "Harm&Ease",
+        time:"7:30 pm",
+        genre: "Rock & Roll",
+        popularTune: "Push & Shove"
+    },
+    {
+        photo: "http://harmandease.com/wp-content/uploads/2022/09/DSC9247-1-1-scaled.jpg",
+        name: "Harm&Ease",
+        time:"7:30 pm",
+        genre: "Rock & Roll",
+        popularTune: "Push & Shove"
+    },
+    {
+        photo: "http://harmandease.com/wp-content/uploads/2022/09/DSC9247-1-1-scaled.jpg",
+        name: "Harm&Ease",
+        time:"7:30 pm",
+        genre: "Rock & Roll",
+        popularTune: "Push & Shove"
     }
 ]
-const renderArtists = () => {
-    
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const artistsContainer = document.getElementById('artistsContainer');
+
+    const renderArtists = () => {
+        artists.forEach((artist) => {
+            const artistItem = document.createElement('div');
+            artistItem.className = 'artist';
+            artistItem.innerHTML = `
+                <img src=${artist.photo} alt="Harm&Ease" > 
+                <h3>${artist.name}</h3>
+                <p>${artist.time}</p>
+                <p>${artist.genre}</p>
+                <p>${artist.popularTune}</p>
+                `;
+
+            const addButton = document.createElement('button');
+            addButton.textContent = 'Buy Ticket';
+            artistItem.appendChild(addButton);
+            artistsContainer.appendChild(artistItem);
+        })
+    };
+
+renderArtists();
+
+});
